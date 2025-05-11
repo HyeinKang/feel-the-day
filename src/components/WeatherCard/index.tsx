b8cd927797fba6bc09f2209c9034c747ea5ddb32
Hyein Kang <17021945+HyeinKang@users.noreply.github.com>
2025-05-12 00:40:26 +0900

    Update weather card with updated sub components

--- src/components/WeatherCard/index.tsx
+++ src/components/WeatherCard/index.tsx
@@ -1,94 +1,107 @@
+import React, { useMemo } from "react";
+import { type Coordinates } from "@/types";
 import {
-  type Coordinates,
-  type WeatherDetail,
-  type DailyForecast,
-  type HourlyForecast,
-} from "@/types/weather";
+  type CurrentWeatherCardData,
+  type DailyWeatherCardData,
+  type HourlyForecastCardData,
+} from "@/types";
+import {
+  type OneCallWeatherResponse,
+  type TimemachineResponse,
+} from "@/types/api/weather";
+
+import {
+  formatCurrentWeather,
+  formatDailyForecasts,
+  formatHourlyForecasts,
+} from "@/utils/formatWeatherData";
+
 import WeatherSummary from "./WeatherSummary";
 import DailyForecastCard from "./DailyForecastCard";
 import HourlyForecastCard from "./HourlyForecastCard";
 
-import WindIcon from "@/assets/icons/Wind.svg";
-import HumidityIcon from "@/assets/icons/Humidity.svg";
-import UVIcon from "@/assets/icons/UV.svg";
-import SunnyCloudyIcon from "@/assets/icons/SunnyCloudy.svg";
-
 interface WeatherCardProps {
-  coordinates: Coordinates | null;
+  coordinates: Coordinates;
+  weatherData: OneCallWeatherResponse | null;
+  yesterdayWeatherData: TimemachineResponse | null;
+  isLoading: boolean;
+  error: Error | null;
 }
 
-const WeatherCard: React.FC<WeatherCardProps> = ({ coordinates }) => {
-  const weatherDetails: WeatherDetail[] = [
-    { label: "Humidity", value: "60%", icon: HumidityIcon },
-    { label: "Wind", value: "5 km/h", icon: WindIcon },
-    { label: "UV Index", value: "3", icon: UVIcon },
-  ];
+const WeatherCard: React.FC<WeatherCardProps> = ({
+  weatherData,
+  yesterdayWeatherData,
+  isLoading,
+  error,
+}) => {
+  const currentWeather: CurrentWeatherCardData | null = useMemo(() => {
+    if (!weatherData) return null;
+    return formatCurrentWeather(weatherData);
+  }, [weatherData]);
 
-  const dailyForecasts: DailyForecast[] = [
-    {
-      dayLabel: "Yesterday",
-      temperature: "22°",
-      feelsLike: "22°",
-      highLow: "H: 25° L: 11°",
-      icon: SunnyCloudyIcon,
-    },
-    {
-      dayLabel: "Tomorrow",
-      temperature: "24°",
-      feelsLike: "27°",
-      highLow: "H: 25° L: 11°",
-      icon: SunnyCloudyIcon,
-    },
-    {
-      dayLabel: "Day After Tomorrow",
-      temperature: "25°",
-      feelsLike: "18°",
-      highLow: "H: 25° L: 11°",
-      icon: SunnyCloudyIcon,
-    },
-  ];
+  const dailyForecasts: DailyWeatherCardData[] = useMemo(() => {
+    if (!weatherData) return [];
+    return formatDailyForecasts(weatherData, yesterdayWeatherData);
+  }, [weatherData, yesterdayWeatherData]);
 
-  const hourlyForecasts: HourlyForecast[] = [
-    { timeLabel: "Now", temperature: "23°", icon: SunnyCloudyIcon },
-    ...Array.from({ length: 11 }).map((_, index) => ({
-      timeLabel: `${String(index + 1)} PM`,
-      temperature: "23°",
-      icon: SunnyCloudyIcon,
-    })),
-  ];
+  const hourlyForecasts: HourlyForecastCardData[] = useMemo(() => {
+    if (!weatherData) return [];
+    return formatHourlyForecasts(weatherData);
+  }, [weatherData]);
+
+  if (isLoading) {
+    return (
+      <div className="absolute top-16 bottom-16 right-10 z-10 w-1/2 bg-white p-4 rounded-xl flex items-center justify-center">
+        Loading...
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="absolute top-16 bottom-16 right-10 z-10 w-1/2 bg-white p-4 rounded-xl flex items-center justify-center">
+        Failed to load weather: {error.message}
+      </div>
+    );
+  }
+
+  if (!currentWeather) {
+    return null; // In case no data
+  }
 
   return (
     <div className="absolute top-16 bottom-16 right-10 z-10 w-1/2 overflow-y-auto bg-[#ffffff80] backdrop-blur-[12px] px-4 py-8 rounded-xl">
-      <h2 className="text-xl text-center mb-4 font-medium">Seoul</h2>
-      {coordinates && (
-        <p className="text-sm text-center">
-          {`Latitude: ${coordinates.lat.toFixed(2)}, Longitude: ${coordinates.lng.toFixed(2)}`}
-        </p>
-      )}
-      <p className="text-sm text-center">
+      {/* Location + Coordinates */}
+      <h2 className="text-xl text-center mb-4 font-medium">
+        City name: {/* {currentWeather.cityName} */}
+      </h2>
+      {/* <p className="text-sm text-center">
         It is 5° higher and feels 1° higher than yesterday.
       </p>
       <p className="text-sm text-center">
         It feels higher because of high Humidity and strong UV index.
-      </p>
+      </p> */}
 
       {/* Weather Summary */}
       <WeatherSummary
-        icon={SunnyCloudyIcon}
-        temperature="23°"
-        feelsLike="25°"
-        highLow="H: 25° L: 11°"
-        details={weatherDetails}
+        weather={currentWeather.weather}
+        iconUrl={currentWeather.iconUrl}
+        currentTemp={currentWeather.currentTemp}
+        feelsLike={currentWeather.feelsLike}
+        // highLow={currentWeather.highLow}
+        details={currentWeather.details}
       />
 
       {/* Daily Forecasts */}
-      <div className="flex justify-between mt-8">
-        {dailyForecasts.map((forecast, index) => (
-          <DailyForecastCard key={index} {...forecast} />
-        ))}
-      </div>
+      <section className="mt-8">
+        <div className="flex justify-between">
+          {dailyForecasts.map((forecast, index) => (
+            <DailyForecastCard key={index} {...forecast} />
+          ))}
+        </div>
+      </section>
 
-      {/* Hourly Forecast */}
+      {/* Hourly Forecasts */}
       <section className="mt-8">
         <hr className="border-t border-gray-300 mb-4" />
         <div className="flex flex-start gap-x-6 py-2 overflow-x-auto">
