1088761ea2c886c8cf61f3cb0c81fb95ef2f7473
Hyein Kang <17021945+HyeinKang@users.noreply.github.com>
2025-05-12 23:47:27 +0900

    Display `now` with only with current hour

--- src/components/WeatherCard/_helpers/formatTime.ts
+++ src/components/WeatherCard/_helpers/formatTime.ts
@@ -2,10 +2,8 @@ export function formatTimestampToLocalTimeLabel(timestamp: number): string {
   const forecastDate = new Date(timestamp * 1000);
   const now = new Date();
 
-  const timeDifference = Math.abs(now.getTime() - forecastDate.getTime());
-  const differenceInMinutes = timeDifference / 1000 / 60;
-
-  if (differenceInMinutes <= 30) {
+  const differenceInHours = Math.abs(now.getHours() - forecastDate.getHours());
+  if (differenceInHours === 0) {
     return "Now";
   }
 
