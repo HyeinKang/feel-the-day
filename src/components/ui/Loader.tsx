fa48bc6401f4b2cc3927e2a536a58cd2b712ce26
Hyein Kang <17021945+HyeinKang@users.noreply.github.com>
2025-05-14 07:52:11 +0900

    Add lighter loader

--- src/components/ui/Loader.tsx
+++ src/components/ui/Loader.tsx
@@ -1,4 +1,4 @@
 const Loader: React.FC = () => (
-  <div className="w-4 h-4 rounded-full flex-shrink-0 bg-cyan-700 dark:bg-cyan-100 animate-[ping_1s_ease-in-out_infinite]" />
+  <div className="w-4 h-4 rounded-full flex-shrink-0 bg-cyan-600 dark:bg-cyan-100 animate-[ping_1s_ease-in-out_infinite]" />
 );
 export default Loader;
