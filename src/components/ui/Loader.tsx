cf3e1a5a7ac7910580e222710acf39f5e7f6afc1
Hyein Kang <17021945+HyeinKang@users.noreply.github.com>
2025-05-14 04:23:11 +0900

    Update the loader

--- src/components/ui/Loader.tsx
+++ src/components/ui/Loader.tsx
@@ -1,4 +1,4 @@
 const Loader: React.FC = () => (
-  <div className="w-4 h-4 rounded-full flex-shrink-0 bg-cyan-700 animate-bounce" />
+  <div className="w-4 h-4 rounded-full flex-shrink-0 bg-cyan-700 animate-[ping_1s_ease-in-out_infinite]" />
 );
 export default Loader;
