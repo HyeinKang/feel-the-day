b72e468f00f720b3ac561db2cbef43c6067f30be
Hyein Kang <17021945+HyeinKang@users.noreply.github.com>
2025-05-14 02:54:43 +0900

    Improved page structure

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,28 +1,67 @@
+import { Sun, X } from "lucide-react";
+
 import { useCoordinates } from "@/hooks/useCoordinates";
+import { useUnit } from "@/hooks/useUnit";
 
 import Map from "@/components/Map";
 import WeatherCard from "@/components/WeatherCard";
 import LocationSearchBar from "@/components/LocationSearchBar";
+import { TemperatureUnitSwitch } from "@/components/TemperatureUnitSwitch";
 
 function Main() {
-  const { coordinates } = useCoordinates();
+  const { coordinates, setCoordinates } = useCoordinates();
+  const { unitSystem, setUnitSystem } = useUnit();
 
   return (
     <>
       <div className="flex flex-col h-screen bg-background-primary">
-        <header className="flex flex-col gap-y-2 absolute top-0 z-10 p-4 bg-white">
-          <h2 className="text-xl font-semibold text-primary">
-            Welcome to the Map
-          </h2>
-          <p className="text-sm text-gray-500">
-            Click on the map to get the coordinates
-          </p>
-          <p className="text-sm text-gray-500">or search for a location</p>
-          <LocationSearchBar />
-        </header>
         <main className="relative flex-grow">
           <Map />
-          {coordinates && <WeatherCard />}
+          <div className="absolute top-4 right-4 z-10 w-full md:w-1/2 max-w-[480px] max-h-[90dvh] flex flex-col gap-y-6 bg-[#eaeaea80] backdrop-blur-[24px] px-6 py-6 rounded-xl">
+            <header className="flex flex-col gap-y-4">
+              <div className="flex items-center justify-between">
+                <h1 className="flex gap-x-2 items-center justify-start font-medium text-xl">
+                  <Sun size={28} className="text-orange-400" />
+                  Feel the day
+                </h1>
+                {coordinates && (
+                  <TemperatureUnitSwitch
+                    onChange={setUnitSystem}
+                    value={unitSystem}
+                  />
+                )}
+              </div>
+              {!coordinates && (
+                <>
+                  <h2 className="">
+                    A weather app that helps you{" "}
+                    <span className="italic">feel the day</span> by comparing
+                    today’s temperature to yesterday’s.
+                  </h2>
+                  <hr />
+                  <p className="text-gray-500">
+                    Click on the map or search for a location to get started.
+                  </p>
+                </>
+              )}
+              <div className="flex items-center gap-x-2">
+                <LocationSearchBar />
+                {coordinates && (
+                  <button
+                    type="button"
+                    aria-label="Close"
+                    className="p-2 rounded-full hover:bg-gray-300 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-gray-400 cursor-pointer"
+                    onClick={() => {
+                      setCoordinates(null);
+                    }}
+                  >
+                    <X size={24} />
+                  </button>
+                )}
+              </div>
+            </header>
+            {coordinates && <WeatherCard />}
+          </div>
         </main>
         <footer className="z-10 w-full bg-white p-2 text-sm text-center">
           <p>© Feel the day, Hyein Kang</p>
